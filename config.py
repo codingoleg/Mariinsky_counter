cb520e2f87da6e290683d8898194b55ce2b69c79
Oleg <codingoleg@proton.me>
2023-07-02 08:01:12 +0300

    Feat: Moved paths from main.py to config.py

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ from os import environ
 from auxilary import validate_dates
 
 # Date in format 'DD.MM.YYYY'
-START_DATE = '01.04.2023'
-FINAL_DATE = '30.04.2023'
+START_DATE = '01.06.2023'
+FINAL_DATE = '30.06.2023'
 validate_dates(START_DATE, FINAL_DATE)
 DATES = START_DATE + '-' + FINAL_DATE
 CODES_PATH = f'./data/{DATES}/codes/'
+JSON_PATH = f'./data/{DATES}/json/'
+CSV_PATH = f'./data/{DATES}/csv/'
 USERNAME = environ['username']
 PASSWORD = environ['password']
 URL_LOGIN = environ['url_login']
