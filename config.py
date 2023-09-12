6022f41b0f3efee01d68497ccc1f5ed1b54ac18f
Oleg <codingoleg@proton.me>
2023-09-12 13:35:47 +0300

    Refactor: config.py

--- config.py
+++ config.py
@@ -1,15 +1,40 @@
 from os import environ
-from auxilary import validate_dates
+from utils import validate_dates
 
-# Date in format 'DD.MM.YYYY'
-START_DATE = '01.06.2023'
-FINAL_DATE = '30.06.2023'
-validate_dates(START_DATE, FINAL_DATE)
-DATES = START_DATE + '-' + FINAL_DATE
-CODES_PATH = f'./data/{DATES}/codes/'
-JSON_PATH = f'./data/{DATES}/json/'
-CSV_PATH = f'./data/{DATES}/csv/'
+# Account
 USERNAME = environ['username']
 PASSWORD = environ['password']
 URL_LOGIN = environ['url_login']
 URL_EVENT = environ['url_event']
+
+# Dates
+start_date = '01.08.2023'
+final_date = '31.08.2023'
+validate_dates(start_date, final_date)
+dates = start_date + '-' + final_date
+
+# Paths
+codes_path = f'./data/{dates}/codes/'
+json_path = f'./data/{dates}/json/'
+csv_path = f'./data/{dates}/csv/'
+xls_path = f'./data/{dates}/xls/'
+workbook_path = './templates/excel/template.xlsx'
+
+# Event types
+BALLET = 'ballet'
+EXTRAS = 'extras'
+
+# Participation types
+FEAT = 'feat'
+SECURE = 'secure'
+
+# Action types
+PERFS = 'perfs'
+REHS = 'rehs'
+
+# Gender
+MEN = 'men'
+WOMEN = 'women'
+
+# For TQDM
+bar_format = '{desc}:|{bar}|{percentage:3.0f}% {n_fmt}/{total_fmt} [{elapsed}]'
