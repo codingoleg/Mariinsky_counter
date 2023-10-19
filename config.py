aaf7e9e5fe0060d5c48c537079e514ab46310148
Oleg <codingoleg@proton.me>
2023-10-19 20:21:12 +0300

    Add: data for db and dates.

--- config.py
+++ config.py
@@ -7,6 +7,14 @@ URL_LOGIN = os.environ['url_login']
 URL_EVENT = os.environ['url_event']
 
 # Dates
+YEAR = 2023
+MONTH = 4
 START_DATE = '01.09.2023'
 FINAL_DATE = '30.09.2023'
 
+# DB
+DB_USER = 'postgres'
+DB_PASSWORD = 'admin'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'db'
